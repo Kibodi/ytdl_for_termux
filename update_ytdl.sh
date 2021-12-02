85e5021986a0788254020cacf846d035555c8878
BelaDerDev <49236169+BelaDerDev@users.noreply.github.com>
2021-12-02 16:42:25 +0100

    Update update_ytdl.sh

--- update_ytdl.sh
+++ update_ytdl.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
 apt update && apt -y upgrade
 python -m pip install --upgrade pip
-pip install --upgrade ffmpeg-python ffmpeg
+pip install --upgrade ffmpeg-python ffmpeg wheel
 python3 -m pip install --no-deps --upgrade -U yt-dlp
